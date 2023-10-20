cfd1cb0bf2721747ddfbc10197b761b9e7019ed5
Jackwillman <jlrscaini@yahoo.com.br>
2023-10-19 23:45:45 -0300

    Web: Embelished ExistingPage type creation

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -1,3 +1,11 @@
-export type ExistingPage = 'Home' | 'About' | 'Dashboard' | 'Login' | 'Signup' | 'AccountCreated' | 'LoggedOut';
+export type ExistingPage = 
+    'Home' | 
+    'About' | 
+    'Dashboard' | 
+    'Login' | 
+    'Signup' | 
+    'AccountCreated' | 
+    'LoggedOut'
+;
 export type SetCookie = (name : string, value : any, options?: any) => void;
 export type CookiesType = { [x: string] : any };
